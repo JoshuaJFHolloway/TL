5d649a40a329cfd0c6891bcb26c2bf27e4bea741
Joshua Holloway <josh.holloway@live.co.uk>
2018-08-11 09:12:22 +0100

    test passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import departingTrains from '../data/departingTrains';
+import toBSK from '../data/toBSK';
+import toSHP from '../data/toSHP';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            departingTrains: departingTrains,
+            toBSK: toBSK,
+            toSHP: toSHP
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
 }
 
 export default App;
