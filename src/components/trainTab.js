8e995d4d29a9fe7e006afcbcef123d275c8239aa
Joshua Holloway <josh.holloway@live.co.uk>
2018-08-13 00:01:15 +0100

    deleted button as its now replaced with font awesome icon with routing

--- src/components/trainTab.js
+++ src/components/trainTab.js
@@ -1,28 +1,53 @@
 import React from 'react';
-import Button from './Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import {
+    Link
+} from 'react-router-dom'
 
-const trainTab = (props) => {
+const TrainTab = (props) => {
+
+    const greenOrNormal = props.actualArrivalTime === "On time" ? {color: "green"} : '';
 
     return (
         <div>
-            <div>
-                {props.scheduledArrivalTime}
+            <div className='arrivalTime'>
+                <div className="inline">
+                    {props.scheduledArrivalTime}
+                </div>
             </div>
-            <div>
-                {props.station}
-                {props.trainCompany}
+            <div className="destination">
+                <div className="inline">
+                    {props.destination}
+                </div>
             </div>
-            <div>
-                {props.platform}
-                {props.actualArrivalTime}
+            <div className="platform">
+                <div className="inline">
+                    {props.platform}
+                </div>
             </div>
-            <div>
-                <Button
-                    onClick={props.onClick}
-                />
+            <div className="callingPointsButton">
+                <div className="inline">
+                    <Link to={`/${props.destination}`}>
+                        <FontAwesomeIcon icon={faAngleRight}/>
+                    </Link>
+                </div>
             </div>
+            <div className="trainCompany">
+                <div className="inline">
+                    {props.trainCompany}
+                </div>
+            </div>
+            <div className="actualArrivalTime">
+                <div className="inline">
+                    <div style={greenOrNormal}>
+                         {props.actualArrivalTime}
+                    </div>
+                </div>
+            </div>
+            <br className="clearInlines"/>
         </div>
     );
 };
 
-export default trainTab;
+export default TrainTab;
