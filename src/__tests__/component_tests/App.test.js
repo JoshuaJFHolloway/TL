5edd79c0d34acf87d35f1bd9f21b79646ae3ebe0
Joshua Holloway <josh.holloway@live.co.uk>
2018-08-12 14:52:18 +0100

    deleted test, moving state to departingServices

--- src/__tests__/component_tests/App.test.js
+++ src/__tests__/component_tests/App.test.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import App from '../../components/App';
 import {shallow} from 'enzyme';
-import departingTrains from '../../data/departingTrains';
-import toBSK from '../../data/toBSK';
-import toSHP from '../../data/toSHP';
 
 let app;
-
-const state = {
-    departingTrains: departingTrains,
-    toBSK: toBSK,
-    toSHP: toSHP
-};
 
 describe('App', () => {
 
@@ -23,11 +14,5 @@ describe('App', () => {
 
     it('renders correctly', () => {
         expect(app).toMatchSnapshot();
-    });
-
-    describe('Initializes state', () => {
-        it('initializes state correctly', () => {
-            expect(app.state()).toEqual(state);
-        });
     });
 });
